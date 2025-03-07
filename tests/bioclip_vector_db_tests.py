288f2bd7323287f33f80b829a7f93fc749c7bfff
Sreejith Noopur <sreejithnoopur@mac.mynetworksettings.com>
2025-03-07 17:04:27 -0500

    more tests

--- tests/bioclip_vector_db_tests.py
+++ tests/bioclip_vector_db_tests.py
@@ -4,9 +4,9 @@ from src.bioclip_vector_db.vector_db import BioclipVectorDatabase, HfDatasetType
 
 class TestBioclipVectorDatabase(unittest.TestCase):
 
-    @patch('src.bioclip_vector_db.vector_db.TreeOfLifeClassifier')
-    @patch('src.bioclip_vector_db.vector_db.datasets')
     @patch('src.bioclip_vector_db.vector_db.chromadb')
+    @patch('src.bioclip_vector_db.vector_db.datasets')
+    @patch('src.bioclip_vector_db.vector_db.TreeOfLifeClassifier')
     def setUp(self, mock_chromadb, mock_datasets, mock_classifier):
         self.mock_classifier = mock_classifier.return_value
         self.mock_datasets = mock_datasets
@@ -15,19 +15,20 @@ class TestBioclipVectorDatabase(unittest.TestCase):
         self.mock_chromadb.PersistentClient.return_value = self.mock_client
         self.mock_collection = MagicMock()
         self.mock_client.get_or_create_collection.return_value = self.mock_collection
-
-        self.dataset_type = HfDatasetType.BIRD
         self.collection_dir = "/tmp/vector_db"
-        self.split = "train"
 
-        self.vdb = BioclipVectorDatabase(
-            dataset_type=self.dataset_type,
+
+    def test_init_ok(self):
+        vdb = BioclipVectorDatabase(
+            dataset_type=HfDatasetType.BIRD,
             collection_dir=self.collection_dir,
-            split=self.split
+            split="train"
         )
+        
+        # FIXME(sreejith)
+        self.assertEqual(vdb._classifier, self.mock_classifier)
+        self.assertEqual(vdb._collection_dir, self.collection_dir)
 
-    def test_sample(self):
-        self.assertEqual(1, 1)
 
 if __name__ == '__main__':
     unittest.main()
