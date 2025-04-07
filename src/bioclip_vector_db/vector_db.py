d05b6cf0320a3d9335091213c620fc375f79e8c7
Sreejith Menon <sreejith.menon59@gmail.com>
2025-04-07 12:44:30 -0400

    Fix the return type hints

--- src/bioclip_vector_db/vector_db.py
+++ src/bioclip_vector_db/vector_db.py
@@ -128,7 +128,7 @@ class BioclipVectorDatabase:
             logger.error(e)
             return None
     
-    def _get_prediction(self, index: int) -> Union(dict, None):
+    def _get_prediction(self, index: int) -> Union[dict, None]:
         """ Returns the prediction of the record at the given index as a dictionary or None if an error occurs. """
         scores = self._classifier.predict([self._get_image(index)], Rank.SPECIES)
         scores = sorted(scores, key=lambda item: item['score'], reverse=True)
