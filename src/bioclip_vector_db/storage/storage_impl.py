764109ab96f8ddc12c4682f5c9f098e959c308ed
smenon8 <sreejith_1729@yahoo.co.in>
2025-11-30 13:38:57 -0500

    remove debug statements

--- src/bioclip_vector_db/storage/storage_impl.py
+++ src/bioclip_vector_db/storage/storage_impl.py
@@ -156,7 +156,6 @@ class FaissIvf(StorageInterface):
         else:
             for id, embedding, metadata in zip(ids, embeddings, metadatas):
                 self._add_embedding_to_index(id, embedding, metadata)
-            logger.info(f"Is Index trained? {self._index.is_trained}")
             logger.info(json.dumps(self._writer._get_health(), indent=2))
         
     def query(self, id: str):
